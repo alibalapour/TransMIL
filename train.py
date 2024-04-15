361382d67b6fd45bb558739492da30bd551faaa4
alibalapour <alibalapour93.ab@gmail.com>
2024-04-15 11:52:43 -0700

    update

--- train.py
+++ train.py
@@ -93,10 +93,10 @@ def main(cfg):
         model_paths = [str(model_path) for model_path in model_paths if 'epoch' in str(model_path)]
         for path in model_paths:
             # new_model = model.load_from_checkpoint(checkpoint_path=path, cfg=cfg)
-            new_model.load_state_dict(torch.load(path, map_location=device))
+            model.load_state_dict(torch.load(path, map_location=device))
 
             # print(new_model.model.state_dict())
-            trainer.test(model=new_model, datamodule=dm)
+            trainer.test(model=model, datamodule=dm)
 
 if __name__ == '__main__':
 
